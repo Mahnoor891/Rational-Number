40cc3469065295c4bf087e646400d978b5b55e35
nida179 <nidahafeez569@gmail.com>
2025-09-06 05:20:17 -0700

    Update Vector.java

--- Vector.java
+++ Vector.java
@@ -42,16 +42,57 @@ class Vector implements Cloneable, Iterable<Double> {
 
          return new Vector(newCoords);
       }
-   
+   public Vector sub(Vector other){
+       if (this.length() != other.length()) {
+         throw new IllegalArgumentException(" Vector should have same dimensions");
+      }
+      Vector result = new Vector();
+      for(int i = 0; i<this.length();i++){
+         result.coordinates.add(this.get(i)-other.get(i);
+      }
+      return result;
+   }
+   pubic double dotProduct(Vector other){
+       if (this.length() != other.length()) {
+         throw new IllegalArgumentException(" Vector should have same dimensions");
+      }
+      double result = 0;
+      for(int i =0;i<this.length();i++){
+         result += this.get(i)*other.get(i);
+      }
+      return result;
+   }
+   public Vector crossProduct(Vector other){
+      if(this.length() != 3 || other.length() != 3){
+         throw new IllegalArgumentException("CROSS PRODUCT is defined for 3-Dimensional VECTOR");
+      }
+       if (this.length() != other.length()) {
+         throw new IllegalArgumentException(" Vector should have same dimensions");
+      }
+      double x1 = this.get(0);
+      double y1 = this.get(1);
+      double z1 = this.get(2);
+      double x2 = other.get(0);
+      double y2 = other.get(1);
+      double z2 = other.get(2);
+
+      Vector result = new Vector();
+      result.coordinates.add(y1*z2-z1*y2); // x-component
+      result.coordinates.add(z1*x2-x1*z2); // y-component
+      result.coordinates.add(x1*y2-y1*x2); // z-component
+
+      return result;
 
    public Vector clone() {
       return new Vector(this.coordinates);
    }
 
+   @Override
    public String toString() {
       return this.coordinates.toString();
    }
 
+   @Override
    public boolean equals(Object o) {
       if (this == o) {
          return true;
@@ -63,6 +104,7 @@ class Vector implements Cloneable, Iterable<Double> {
       }
    }
 
+   @Override
    public Iterator<Double> iterator() {
       return this.coordinates.iterator();
    }
